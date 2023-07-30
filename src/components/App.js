bd3971808dc9584792bbef610db43c4d58834c11
Shivani Tiwari <116451611+SheWe786@users.noreply.github.com>
2023-07-30 19:25:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,19 @@ import '../styles/App.css';
 
 const App = () => {
 //code here 
+  const [isBlue, setIsBlue] = useState(false);
+
+  const handleClick = () => {
+    setIsBlue(!isBlue);
+  };
+  
   return (
-    <div id="main">
-      <p className={} >Newton School</p>
-      <button id='button' onClick={}>Change Style</button>
+     <div className="App">
+      <button id="button" onClick={handleClick}>Change Style</button>
+      <p className={isBlue ? 'blueColor' : 'redColor'}>This is some text.</p>
     </div>
   )
 }
 
 
-export default App;
+export default App;
